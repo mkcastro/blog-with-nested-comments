321cdb3edf1341bbe567135e03f66022291fb5c3
Mike Castro <mkcastro24@gmail.com>
2022-03-05 01:55:27 +0800

    create store blog feature

---
+++ tests/Feature/StoreBlogTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class StoreBlogTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_store_blog()
+    {
+        $response = $this->postJson(route('blogs.store'), [
+            'title' => 'Test Title',
+            'body' => 'Test Body',
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('blogs', [
+            'title' => 'Test Title',
+            'body' => 'Test Body',
+        ]);
+    }
+}
