a9a8b1c079c6fef8713ce2eb799ed32dfb2e49bb
Mike Castro <mkcastro24@gmail.com>
2022-03-05 01:58:24 +0800

    force blog title unique

--- database/migrations/2022_03_04_173135_create_blogs_table.php
+++ database/migrations/2022_03_04_173135_create_blogs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title')->unique();
             $table->text('body');
             $table->timestamps();
         });
