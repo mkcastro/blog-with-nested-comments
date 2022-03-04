5f3a818ac0d37bec6179acc2d130caa92aba64cf
Mike Castro <mkcastro24@gmail.com>
2022-03-05 02:06:19 +0800

    seed blogs on local

---
+++ database/seeders/BlogSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Blog;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
+
+class BlogSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (App::isProduction()) {
+            return false;
+        }
+
+        Blog::factory(10)->create();
+    }
+}
