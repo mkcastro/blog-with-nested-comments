92bfa1137c4add5f6297bd7ceca234748bcd1c94
Mike Castro <mkcastro24@gmail.com>
2022-03-05 11:21:04 +0800

    create user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (App::isProduction()) {
+            return false;
+        }
+
+        if (User::whereEmail('john@example.com')->exists()) {
+            return false;
+        }
+
+        User::factory()->create([
+            'email' => 'john@example.com'
+        ]);
+    }
+}
