ffad06d78004bb818ba07645d81488b8343e4616
Mike Castro <mkcastro24@gmail.com>
2022-03-06 19:17:01 +0800

    init render blogs

--- resources/js/Pages/Blog/Index.vue
+++ resources/js/Pages/Blog/Index.vue
@@ -9,7 +9,16 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    Show blogs here
+                    <ul>
+                        <li v-for="(blog, key) in blogs" :key="key">
+                            <Link
+                                :href="route('blogs.show', blog.id)"
+                                as="button"
+                                class="underline text-sm text-gray-600 hover:text-gray-900"
+                                >{{ blog.title }}</Link
+                            >
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -19,10 +28,17 @@
 <script>
 import { defineComponent } from "vue";
 import AppLayout from "@/Layouts/AppLayout.vue";
+import { Link } from "@inertiajs/inertia-vue3";
 
 export default defineComponent({
     components: {
         AppLayout,
+        Link,
+    },
+    computed: {
+        blogs() {
+            return this.$page.props.blogs;
+        },
     },
 });
 </script>
