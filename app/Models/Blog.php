70ee268858df818f5c8213fb7786719e6032fe32
Mike Castro <mkcastro24@gmail.com>
2022-03-08 01:04:39 +0800

    add blog has comments relationship

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 use App\Contracts\HasAuthorInterface;
+use App\Contracts\HasCommentsInterface;
 use App\Traits\HasAuthorTrait;
+use App\Traits\HasCommentsTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Blog extends Model implements HasAuthorInterface
+class Blog extends Model implements HasAuthorInterface, HasCommentsInterface
 {
     use HasFactory;
     use HasAuthorTrait;
+    use HasCommentsTrait;
 }
