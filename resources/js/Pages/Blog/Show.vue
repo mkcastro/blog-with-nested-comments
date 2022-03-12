823d1267eb199c1c2372b36f2a8cee9dd5cf4bce
Mike Castro <mkcastro24@gmail.com>
2022-03-12 22:09:06 +0800

    cleanup show blog ui

--- resources/js/Pages/Blog/Show.vue
+++ resources/js/Pages/Blog/Show.vue
@@ -1,21 +1,29 @@
 <template>
-    <app-layout :title="blog.title">
-        <template #header>
-            <h2
-                class="font-semibold text-xl text-gray-800 leading-tight"
-                v-text="blog.title"
-            ></h2>
-        </template>
+  <app-layout :title="blog.title">
+    <template #header>
+      <h2
+        class="font-semibold text-xl text-gray-800 leading-tight"
+        v-text="blog.title"
+      ></h2>
+    </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div
-                    class="bg-white overflow-hidden shadow-xl sm:rounded-lg"
-                    v-text="blog.body"
-                ></div>
-            </div>
-        </div>
-    </app-layout>
+    <div
+      class="
+        px-6
+        py-10
+        w-full
+        mx-auto
+        prose
+        dark:prose-invert
+        h-fit
+        prose-img:mx-auto
+      "
+    >
+      <h1 v-text="blog.title"></h1>
+
+      <p v-text="blog.body"></p>
+    </div>
+  </app-layout>
 </template>
 
 <script>
@@ -24,14 +32,14 @@ import AppLayout from "@/Layouts/AppLayout.vue";
 import Welcome from "@/Jetstream/Welcome.vue";
 
 export default defineComponent({
-    components: {
-        AppLayout,
-        Welcome,
+  components: {
+    AppLayout,
+    Welcome,
+  },
+  computed: {
+    blog() {
+      return this.$page.props.blog;
     },
-    computed: {
-        blog() {
-            return this.$page.props.blog;
-        },
-    },
+  },
 });
 </script>
